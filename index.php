46374d44da2d58691ef0c3735da018149abd4ec4
josealfredojimenezgarcia <joseajimenez07@gmail.com>
2021-04-28 17:36:37 -0500

    Decrece e incrementa

---
+++ index.php
@@ -0,0 +1,125 @@
+<?php
+
+    echo "<h1>Operadores Aritméticos <a href='https://www.php.net/manual/es/language.operators.arithmetic.php' target='_black'>Pagina</a> </h1>";
+
+     $i = (int) 7;
+     $x = (int) 5;
+
+    $i++;  
+    $i++;
+    $i++;
+    $i++;
+    $i--;
+    $i--; 
+    $i++; 
+    $i++; 
+    $i++; 
+    $i++; 
+    $i++; 
+    $i++; 
+    $i++; 
+    $i++; 
+    echo 'Valos de x: ',$x;;
+    echo "<br>";
+    echo 'Valos de i: ',$i;
+    echo "<br>";
+    $x--;
+    $x--;
+    $x--;
+    $x++;
+    $x++;
+    echo 'Valos de x: ',$x;;
+    echo "<br>";
+    echo 'Valos de i: ',$i;
+    ++$x;
+    ++$x;
+    --$x;
+    ++$x;
+    --$x;
+    --$i;
+    ++$i;
+    --$i;
+    ++$i;
+    --$i;
+    ++$i;
+    ++$i;
+    ++$i;
+    ++$i;
+    
+    echo "<br>";
+    // Operador Post-Decremento
+    // $x--;
+    // echo $x;
+    // Operador Pre-Decremento
+    // echo --$x;
+    echo 'Valos de x: ',$x;
+    echo "<br>";
+    echo 'Valos de i: ',$i;
+    // Operador Post-Incremento
+    // $i++;
+    // echo $i;
+    // Operador Pre-Incremento
+    // echo --$i;
+
+
+    // $num1 = (int) 10;
+    // $num2 = (int) 3;
+    // Ejemplos
+
+    // echo ($num1 + $num2) + (($num1 - $num2) + 2) * 10;
+    // echo $num1 + $num2 * 2;
+    // echo "<br>";
+    // echo ($num1 + $num2) * 2;
+    // echo "<br>";
+    // echo (1-($num1 + $num2)) * 2;
+    // echo "<br>";
+    // echo 2 * $num1 + $num2;
+
+    
+    // Operador Potencia
+    // $resultado = (int) $num1 ** $num2;
+    // echo $resultado;
+    //Abrebiado 
+    // $num1 **= $num2;
+    // echo $num1;
+
+
+    // Operador Modulo
+    // $resultado = (int) $num1 % $num2;
+    // echo $resultado;
+    // Abrebiado
+    // $num1 %= $num2;
+    // echo $num1;
+
+
+    // Operador Division
+    // $resultado = (int) $num1 / $num2;
+    // echo $resultado;
+    // Abrebiado 
+    // $num1 /= $num2;
+    // echo $num1;
+
+    // Operador Multiplicacion
+    // Significado 
+    // $resultado = (int) $num1 * $num2;
+    // echo $resultado;
+    // Abrebiado 
+    // $num1 *= $num2;
+    // echo $num1;
+
+    // Operador Resta
+    // Significado 
+    // $resultado = (int) $num1 - $num2;
+    // echo $resultado;
+    // Abrebiado
+    // $num1 -= $num2;
+    // echo $num1;
+
+    // Operador Suma
+    // Significado 
+    // $resultadoDeLaSuma = (int) $num1 + $num2;
+    // echo $resultadoDeLaSuma; 
+    // Abrebiado
+    // $num1 += $num2;
+    // echo $num1;
+?>
